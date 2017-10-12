a6bb9cb8f8663e648e9ab87ec409148db3400aee
COREY COSMAN <cosman.corey@gmail.com>
2017-10-12 16:27:03 -0400

    set up routes get route for index page, setup up pictures controller, set up model for pictures, set up routes for adding, updating, and deleting a picture

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,54 @@
+class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def create
+
+    @picture = Picture.new
+    @picture.artist = params[:picture][:artist]
+    @picture.title = params[:picture][:title]
+    @picture.url = params[:picture][:url]
+    if @picture.save
+      redirect_to '/pictures'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.artist = params[:picture][:artist]
+    @picture.title = params[:picture][:title]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Picture.create(
+  artist: "artist one",
+  title: "title_one",
+  url: "http://unsplash.it/200/300?random="
+)
+
+Picture.create(
+  artist: "artist two",
+  title: "title_two",
+  url: "http://unsplash.it/200/300?random="
+)
+
+Picture.create(
+  artist: "artist three",
+  title: "title_three",
+  url: "http://unsplash.it/200/300?random="
+)
+
+Picture.create(
+  artist: "artist four",
+  title: "title_four",
+  url: "http://unsplash.it/200/300?random="
+)
